f76b1f36c92a1851544f3c36b975f9d794e13f29
thushanm <122750947+thushanm@users.noreply.github.com>
2024-02-23 23:07:30 +0530

    Making Coler Picker Component

---
+++ src/ColorPicker.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+
+export function ColorPicker(){
+const [color,setColor]=useState("#FFFFFF")
+    return(<div className="color-picker-div">
+            <h1>Color Picker</h1>
+
+        </div>
+
+    );
+}
