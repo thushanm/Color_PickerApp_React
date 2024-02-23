93167b177e3adbd0e7f8886592a8e5838897c93a
thushanm <122750947+thushanm@users.noreply.github.com>
2024-02-23 23:10:42 +0530

    add color

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -4,6 +4,12 @@ export function ColorPicker(){
 const [color,setColor]=useState("#FFFFFF")
     return(<div className="color-picker-div">
             <h1>Color Picker</h1>
+            <div className="color-display" style={{backgroundColor:color}}>
+
+                <p>Select Color</p>
+            </div>
+            <label>Select Color</label>
+            <input/>
 
         </div>
 
