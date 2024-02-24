f8a683258c8a60da52c5ace951caffb34eb847ae
thushanm <122750947+thushanm@users.noreply.github.com>
2024-02-24 15:23:43 +0530

    Dark Mood Oky

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,18 +1,30 @@
 import {useState} from "react";
 
 export function ColorPicker(){
-const [color,setColor]=useState("#FFFFFF")
-    function handleColorChange(e){
-    setColor(e.target.value);
+
+
+    const [color,setColor]=useState("#FFFFFF")
+
+    function changeBackground(e){
+        {document.getElementById('mainDiv').style.backgroundColor="#0a0a0a"}
+        {document.getElementById('h1Main').style.color="#ffffff"}
+        {document.getElementById('pMain').style.color="#ffffff"}
+        {document.getElementById('btnColor').style.backgroundColor="#ffffff"}
     }
-    return(<div className="color-picker-div">
-            <h1>Color Picker</h1>
+    function handleColorChange(e){
+        setColor(e.target.value);
+    }
+    return(<div className="color-picker-div" id="mainDiv">
+            <h1 id="h1Main">Color Picker</h1>
             <div className="color-display" style={{backgroundColor:color}}>
 
-                <p>Select Color</p>
+                <p id="pMain">Select Color</p>
             </div>
             <label>Select Color</label>
             <input type="color" value={color} onChange={handleColorChange}/>
+            <div>
+            </div>
+            <button id="btnColor" onClick={changeBackground}>Change Color</button>
 
         </div>
 
